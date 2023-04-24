a07f332f62bf7e7c84355fc498eb9241beb84e2a
이유정 <pp89925244@gmail.com>
2023-04-24 19:06:05 +0900

    cart 모듈화

--- app/cart/page.js
+++ app/cart/page.js
@@ -2,11 +2,7 @@ export default function Cart() {
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
+        <CartItem />
       </div>
     )
   } 
