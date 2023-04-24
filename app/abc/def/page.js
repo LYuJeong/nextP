cb842c294011828b2c4fa7ad41e0b08c37770b67
이유정 <pp89925244@gmail.com>
2023-04-24 16:10:54 +0900

    first

---
+++ app/abc/def/page.js
@@ -0,0 +1,10 @@
+
+export default function abc() {
+
+    return (
+      <div>
+        <h2 className="title">def page</h2>
+      </div>
+    )
+  }
+  
---
+++ app/abc/page.js
@@ -0,0 +1,9 @@
+
+export default function abc() {
+
+  return (
+    <div>
+      <h2 className="title">라우팅테스트</h2>
+    </div>
+  )
+}
