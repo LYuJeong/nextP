4f2b07eac5d7b56e8feadb4cad6d54b59fb0d051
이유정 <pp89925244@gmail.com>
2023-04-25 16:19:07 +0900

    today

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,7 +1,16 @@
+import { age, name} from './data.js'
+
 export default function Cart() {
     return (
       <div>
         <h4 className="title">Cart</h4>
+        <div className="cart-item">
+            <p>상품명 {age}</p>
+            <p>$40</p>
+            <p>1개</p>
+            +
+          </div>
+        <CartItem />
         <CartItem />
       </div>
     )
